f4e74069493854666a811b6c662484cf4bbc810c
Hafiizh Ghulam <hafiizh.ghulam@gmail.com>
2023-01-12 17:27:38 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/projekkel3_bismillahbangget/AddRecipesActivity.kt
@@ -0,0 +1,12 @@
+package com.example.projekkel3_bismillahbangget
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddRecipesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_recipes)
+    }
+}
---
+++ app/src/main/java/com/example/projekkel3_bismillahbangget/SettingActivity.kt
@@ -0,0 +1,27 @@
+package com.example.projekkel3_bismillahbangget
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        getSupportActionBar()?.setDisplayHomeAsUpEnabled(true)
+        val move: Button = findViewById(R.id.aboutus)
+        move.setOnClickListener{
+            val moveaboutus = Intent(this, AboutUsActivity::class.java)
+            startActivity(moveaboutus)
+        }
+    }
+}
